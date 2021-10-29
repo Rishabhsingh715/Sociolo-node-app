c0be50cbc1f2eac11f2dc328f999f37a18f4e388
Rishabhsingh715 <srishabh715@gmail.com>
2021-10-29 14:48:59 +0530

    added the sign up feature

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 8002;
+const cookieParser = require('cookie-parser');
+const db = require('./config/mongoose');
 
-app.use('/', require('./routes'));
 
 //set up the view engine
 app.set('view engine', 'ejs');
 app.set('views','./views');
+
+app.use(express.urlencoded());
+app.use(cookieParser());
+
+
+app.use('/', require('./routes'));
 
 
 app.listen(port,function(err){
